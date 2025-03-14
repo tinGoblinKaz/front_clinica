033d9fdf81004da3ba376b6d1b24fbe68d731983
Programador Full Stack 36133232024.3 <36133232024.3@senacrjedu.local>
2025-03-13 21:31:41 -0300

    Subindo novo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FooterComponent, HeaderComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'clinica';
 }
