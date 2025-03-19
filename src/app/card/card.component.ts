673f42983ef4158042d53318c694d87a1397698e
Programador Full Stack 36133232024.3 <36133232024.3@senacrjedu.local>
2025-03-18 21:30:35 -0300

    subindo modificações do dia 18_03_25

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,6 +10,7 @@ export class CardComponent {
   medico ={
     nome: "Dr. Matheus Marques",
     especialidade:"Cardiologia",
-
+    imagem:"../../assets/user-1.jpg",
+    imagem2:"https://cpv.ifsp.edu.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"
   }
 }
